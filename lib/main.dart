0a6df85d5c8a61b3e9a6e5111bb2af0160ccafc7
Shobhit Singh <majin.shobhitrawat53@gmail.com>
2024-08-22 21:17:18 +0530

    firebase implemented have to resolve the issues that comes now

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_storage_application/ui/splashScreen.dart';
 import 'package:photo_storage_application/utils/shared_pref.dart';
@@ -5,7 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp();
   // Initialize SharedPreferences
   await SharedPrefsService.instance.init();
 
